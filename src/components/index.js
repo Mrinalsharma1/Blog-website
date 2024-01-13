b67e8bab1d3d141e835f335f0d7fa30a5e6a2d58
Mrinalsharma1 <checkkro1@gmail.com>
2024-01-13 18:22:31 +0530

    implemented real time editor and postform component with complex logic

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from '../components/container/container'
 import Logo from "./Logo";
+import RTE from "./RTE";
 
-export { Header, Footer, Logo, LogoutBtn, Container }
+export { Header, Footer, Logo, LogoutBtn, Container, RTE }
